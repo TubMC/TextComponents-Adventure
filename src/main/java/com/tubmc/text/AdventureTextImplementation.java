eb38e9392d9cc2d034c54fba85fd59dc100bf03e
BradBot_1 <BradBot_1@outlook.com>
2024-02-06 00:39:40 +0000

    licensing

--- src/main/java/com/tubmc/text/AdventureTextImplementation.java
+++ src/main/java/com/tubmc/text/AdventureTextImplementation.java
@@ -16,6 +16,21 @@ import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.TranslatableComponent;
 import net.kyori.adventure.text.format.TextDecoration;
 
+/**
+ *    Copyright 2023-2024 TubMC.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 @Internal
 final class AdventureTextImplementation extends AbstractImplementation {
 
